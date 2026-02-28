66631f03eeb930b6a317f6c13d1ae072c82bacbc
Omralejandro22 <omar-poot@outlook.com>
2026-02-28 15:55:41 -0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 3306,
+    port: process.env.DB_PORT || 10468,
     ssl: {
         rejectUnauthorized: false
     },
@@ -16,4 +16,5 @@ const pool = mysql.createPool({
 });
 
 module.exports = pool;
+
 
